2612983a3d6c94568191ab6a44dd29fd4b6cdcc5
Davi Abdallah <5629913+daviabdallah@users.noreply.github.com>
2023-10-17 17:08:01 -0400

    Update udf-databricks-cluster-multiprocessing.py

--- udf-databricks-cluster-multiprocessing/udf-databricks-cluster-multiprocessing.py
+++ udf-databricks-cluster-multiprocessing/udf-databricks-cluster-multiprocessing.py
@@ -16,9 +16,9 @@ iterations = 1000000 * 1000
 
 rdd_sim = sc.parallelize(range(0, iterations))
 
-rdd_sim = rdd_sim.map(lambda iteration: (throw_dart(iteration), iteration))
+rdd_sim_spark = rdd_sim.map(lambda iteration: (throw_dart(iteration), iteration))
 
-hits = rdd_sim.countByKey()[1]
+hits = rdd_sim_spark.countByKey()[1]
 pi = (4 * hits) / iterations
 print(iterations)
 print(pi)
