1994519075f24024b505958f328d1b3553c3de52
Davi Abdallah <5629913+daviabdallah@users.noreply.github.com>
2024-05-23 14:55:21 -0400

    Create start-notebook-in-specific-cluster.py

---
+++ start-notebook-in-specific-cluster/start-notebook-in-specific-cluster.py
@@ -0,0 +1,7 @@
+cluster_config = '''
+{
+  "existing_cluster_id": "cluster id" # get cluster id by running spark.conf.get("spark.databricks.clusterUsageTags.clusterId") at the desired cluster
+}
+'''
+
+dbutils.notebook.run('Notebook path to run', 36000, _NotebookHandler__databricks_internal_cluster_spec=cluster_config)
